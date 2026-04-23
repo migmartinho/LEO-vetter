3c4aa0739b601a8db3eb73f80d81893cf0c573cc
Miguel Martinho <miguel.martinho@nasa.gov>
2026-04-23 11:21:49 -0700

    - Optimizing computation of MES and SES.

--- leo_vetter/main.py
+++ leo_vetter/main.py
@@ -95,29 +95,71 @@ class TCELightCurve:
         bin_flux = np.zeros(N)
         bin_flux_err = np.zeros(N)
         phase = phasefold(self.time, self.per, self.epo)
-        phase[phase < 0] += 1
+        phase[phase < 0] += 1  # shifts phase to 0 to 1 for easier handling
+        dur_half = 0.5 * self.dur
+        left_idx, right_idx = 0, 0
+        time_diff_dur_minus, time_diff_dur_plus = self.time - dur_half, self.time + dur_half
+        phase_sorted_idxs = np.argsort(phase)
+        phase_sorted = phase[phase_sorted_idxs]
+        dur_per_frac_half = 0.5 * self.qtran
         for i in np.arange(N):
-            # Get individual transit depth at this cadence, i.e. only use datapoints close in time
-            in_tran = abs(self.time - self.time[i]) < 0.5 * self.dur
-            n_SES[i] = np.sum(in_tran)
+            # time is sorted
+            while left_idx < N and self.time[left_idx] < time_diff_dur_minus[i]:
+                left_idx += 1
+            if right_idx < left_idx:
+                right_idx = left_idx
+            while right_idx < N and self.time[right_idx] <= time_diff_dur_plus[i]:
+                right_idx += 1
+            
+            n_SES[i] = right_idx - left_idx
             dep_SES[i] = self.zpt - weighted_mean(
-                self.flux[in_tran], self.flux_err[in_tran]
+                self.flux[left_idx:right_idx], self.flux_err[left_idx:right_idx]
             )
-            # Get overall transit depth at this cadence, i.e. use all datapoints close in phase
-            all_tran = (abs(phase - phase[i]) < 0.5 * self.qtran) | (
-                abs(phase - phase[i]) > 1 - 0.5 * self.qtran
-            )
-            n_MES[i] = np.sum(all_tran)
+            
+            # in_bin = (left_idx < np.arange(N)) & (np.arange(N) < right_idx) & ~self.near_tran
+            mask_near_tran = ~self.near_tran[left_idx:right_idx]
+            
+            # Get individual transit depth at this cadence, i.e. only use datapoints close in time
+            # in_tran = abs(self.time - self.time[i]) < 0.5 * self.dur
+            # n_SES[i] = np.sum(in_tran)
+            # dep_SES[i] = self.zpt - weighted_mean(
+            #     self.flux[in_tran], self.flux_err[in_tran]
+            # )
+            
+            # find indices within +- range from the current phase index
+            left_p = np.searchsorted(phase_sorted, phase[i] - dur_per_frac_half, side='left')
+            right_p = np.searchsorted(phase_sorted, phase[i] + dur_per_frac_half, side='right')
+            # also check for wrap around in phase
+            left_p_wrap = np.searchsorted(phase_sorted, phase[i] - dur_per_frac_half + 1, side='left')
+            right_p_wrap = np.searchsorted(phase_sorted, phase[i] + dur_per_frac_half + 1, side='right')
+            
+            n_MES[i] = (right_p - left_p) + (right_p_wrap - left_p_wrap)
             dep_MES[i] = self.zpt - weighted_mean(
-                self.flux[all_tran], self.flux_err[all_tran]
+                np.concatenate((self.flux[phase_sorted_idxs[left_p:right_p]], self.flux[phase_sorted_idxs[left_p_wrap:right_p_wrap]])),
+                np.concatenate((self.flux_err[phase_sorted_idxs[left_p:right_p]], self.flux_err[phase_sorted_idxs[left_p_wrap:right_p_wrap]]))
             )
-            epochs = np.round((self.time - self.time[i]) / self.per)
-            tran_epochs = np.unique(epochs[all_tran])
+            
+            tran_epochs = np.unique(np.concatenate((self.epochs[phase_sorted_idxs[left_p:right_p]], 
+                                                    self.epochs[phase_sorted_idxs[left_p_wrap:right_p_wrap]])))
+            
+            bin_flux[i] = weighted_mean(self.flux[left_idx:right_idx][mask_near_tran], self.flux_err[left_idx:right_idx][mask_near_tran])
+            bin_flux_err[i] = weighted_err(self.flux[left_idx:right_idx][mask_near_tran], self.flux_err[left_idx:right_idx][mask_near_tran])
+            
+            # # Get overall transit depth at this cadence, i.e. use all datapoints close in phase
+            # all_tran = (abs(phase - phase[i]) < 0.5 * self.qtran) | (
+            #     abs(phase - phase[i]) > 1 - 0.5 * self.qtran
+            # )
+            # n_MES[i] = np.sum(all_tran)
+            # dep_MES[i] = self.zpt - weighted_mean(
+            #     self.flux[all_tran], self.flux_err[all_tran]
+            # )
+            # epochs = np.round((self.time - self.time[i]) / self.per)
+            # tran_epochs = np.unique(epochs[all_tran])
             N_transit_MES[i] = len(tran_epochs)
             # Get running mean and uncertainty of out-of-transit fluxes, binned over transit timescale
-            in_bin = in_tran & ~self.near_tran
-            bin_flux[i] = weighted_mean(self.flux[in_bin], self.flux_err[in_bin])
-            bin_flux_err[i] = weighted_err(self.flux[in_bin], self.flux_err[in_bin])
+            # in_bin = in_tran & ~self.near_tran
+            # bin_flux[i] = weighted_mean(self.flux[in_bin], self.flux_err[in_bin])
+            # bin_flux_err[i] = weighted_err(self.flux[in_bin], self.flux_err[in_bin])
         # Estimate white and red noise following Hartman & Bakos (2016)
         mask = ~np.isnan(bin_flux) & ~self.near_tran
         std = weighted_std(self.flux[mask], self.flux_err[mask])
